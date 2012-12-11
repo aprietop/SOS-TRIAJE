4da33fefde3f56842c0feb69922a4669e44f9c9c
prod1 <prod1@salaprod1.med.ucv.ve>
2012-12-11 09:03:59 -0430

    SOS-TRIAJE funcional V111212-v2 PojoArchivo

---
+++ SOS-TRIAJE/src/java/pojos/PojoArchivo.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pojos;
+
+/**
+ *
+ * @author prod1
+ */
+public class PojoArchivo {
+    
+    public String Nombre;
+    public String Descripcion;
+    byte[] Archivo;
+    
+}
